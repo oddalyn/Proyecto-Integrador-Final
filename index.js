0fd0f6d3302c92d7ac10a9751d71dc3f36231fd8
Sam Leonetti <oddalyn@gmail.com>
2024-05-09 20:27:59 -0300

    Estructura del html

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var myModal = new bootstrap.Modal(document.getElementById('modal'), options)
+myModal.toggle()
+myModal.show();
+
+// var myModal = document.getElementById('modal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+
